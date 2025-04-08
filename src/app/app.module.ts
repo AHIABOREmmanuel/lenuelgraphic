4efbaa7302fe4b025fd36af80ee8b42bf312b2bb
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-04-08 12:37:00 +0000

    déployement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
